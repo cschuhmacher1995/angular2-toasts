9c9b668486ad4248178e0b89c7f2d5879cd666e3
Christian Schuhmacher <Christian Schuhmacher>
2016-05-21 12:41:08 +0200

    fix return type for angular update

--- notifications.service.ts
+++ notifications.service.ts
@@ -3,19 +3,19 @@
  */
     
 export declare class NotificationsService {
-    success(title: string, content: string, override?: any)
+    success(title: string, content: string, override?: any): void
 
-    error(title: string, content: string, override?: any)
+    error(title: string, content: string, override?: any): void
 
-    alert(title: string, content: string, override?: any)
+    alert(title: string, content: string, override?: any): void
 
-    info(title: string, content: string, override?: any)
+    info(title: string, content: string, override?: any): void
 
-    bare(title: string, content: string, override?: any)
+    bare(title: string, content: string, override?: any): void
 
-    create(title: string, content: string, type: string, override?: any)
+    create(title: string, content: string, type: string, override?: any): void
 
-    html(html: any, type: string, override?: any)
+    html(html: any, type: string, override?: any): void
 
-    remove(id?: string)
+    remove(id?: string): void
 }
